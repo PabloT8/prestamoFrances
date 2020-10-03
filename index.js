bd51b6444e718227506d51b0c4867123929fa2a6
PabloT <tavarespablo7@gmail.com>
2020-10-03 16:01:15 -0600

    implementacion expresshandlebars como template engine y creacion del endpoint de / con el formulario de prestamo

--- index.js
+++ index.js
@@ -1,14 +1,20 @@
 // Importar express
 const express = require("express");
+// Importar handlebars como template engine
+const exphbs = require("express-handlebars");
 
 // Crear un servidor express
 const app = express();
+
+// Indicar a express utilizar handlebars como template engine
+app.engine("hbs", exphbs({ defaultLayout: "main", extname: ".hbs" }));
+app.set("view engine", "hbs");
 
 // Crear una ruta para /
 // Información sobre los verbos HTTP
 // https://developer.mozilla.org/es/docs/Web/HTTP/Methods
 app.get("/", (req, res, next) => {
-  res.send("bienvenido");
+  res.render("formulario_prestamo");
 });
 
 // Inicializar el servidor en un puerto en específico
